f3ac5506b6445654ae351dd1cc2fd93cb39aac51
mauricio <mtanusmafud@gmail.com>
2023-11-27 20:21:10 -0300

    Edición de datos de proveedor y función para validarlos

--- scripts/crud_proveedores.js
+++ scripts/crud_proveedores.js
@@ -3,12 +3,14 @@ let divResultado = document.querySelector("#resultado-peticion");
 let seccionBuscarProveedor = document.querySelector("#seccion-buscar-proveedor");
 let seccionAgregarProveedor = document.querySelector("#seccion-agregar-proveedor");
 let seccionModificarProveedor = document.querySelector("#seccion-modificar-proveedor");
+let seccionEditarProveedor = document.querySelector("#seccion-editar-proveedor");
 let seccionEliminarProveedor = document.querySelector("#seccion-eliminar-proveedor");
 
 function ocultarPaneles() {
     seccionBuscarProveedor.style.display = "none";
     seccionAgregarProveedor.style.display = "none";
     seccionModificarProveedor.style.display = "none";
+    seccionEditarProveedor.style.display = "none";
     seccionEliminarProveedor.style.display = "none";
     divResultado.innerHTML = '';
 }
@@ -144,46 +146,36 @@ formularioAgregarProveedor.addEventListener('submit', evento => {
     evento.preventDefault();
 
     /* Validar el formulario para crear un nuevo proveedor */
-
-    let datosProveedor = new FormData();
-    let datosValidos = true;
-
-    let telefono = document.querySelector('#form-agregar-proveedor #telefono-prov').value;    
-    if ((!telefono == '') & (!validarTelefonoProveedor(telefono))) {
-        datosValidos = false;
-        document.querySelector("#form-agregar-proveedor #telefono-prov").focus();
-    } else {datosProveedor.append('telefono-prov', telefono);}        
-
-    let cuit = document.querySelector('#form-agregar-proveedor #cuit-prov').value;
-    if (!validarCuitProveedor(cuit)) {
-        datosValidos = false;
-        document.querySelector("#form-agregar-proveedor #cuit-prov").focus();
-    } else {datosProveedor.append('cuit-prov', cuit);}    
-
-    let email = document.querySelector('#form-agregar-proveedor #email-prov').value;
-    if ((!email == '') & (!validarEmailProveedor(email))) {
-        datosValidos = false;
-        document.querySelector("#form-agregar-proveedor #email-prov").focus();
-    } else {datosProveedor.append('email-prov', email);}        
-
-    let direccion = document.querySelector('#form-agregar-proveedor #direccion-prov').value;
-    if ((!direccion == '') & (!validarDireccionProveedor(direccion))) {
-        datosValidos = false;
-        document.querySelector("#form-agregar-proveedor #direccion-prov").focus();
-    } else {datosProveedor.append('direccion-prov', direccion);}    
-
     let nombre = document.querySelector('#form-agregar-proveedor #nombre-prov').value;
-    if (!validarNombreProveedor(nombre)) {
-        datosValidos = false;
-        document.querySelector("#form-agregar-proveedor #nombre-prov").focus();
-    } else {datosProveedor.append('nombre-prov', nombre);}
-        
-    console.log("El nuevo proveedor es:", datosProveedor);
+    let direccion = document.querySelector('#form-agregar-proveedor #direccion-prov').value;
+    let email = document.querySelector('#form-agregar-proveedor #email-prov').value;
+    let cuit = document.querySelector('#form-agregar-proveedor #cuit-prov').value;
+    let telefono = document.querySelector('#form-agregar-proveedor #telefono-prov').value;    
+
+    let datosProveedor = {
+        nombre: nombre,
+        direccion: direccion,
+        email: email,
+        cuit: cuit,
+        telefono: telefono  
+    };
+    console.log("Datos de proveedor a agregar:", datosProveedor);
+
+    let datosValidos = true;
+    datosValidos = validarDatosProveedor(datosProveedor, "agregar");
+    console.log("Validación de datos de proveedor nuevo:", validarDatosProveedor(datosProveedor, "agregar"));
 
     if(datosValidos) {
+        let datosProveedorVerificado = new FormData();
+        datosProveedorVerificado.append('nombre-prov', datosProveedor.nombre);        
+        datosProveedorVerificado.append('direccion-prov', datosProveedor.direccion);        
+        datosProveedorVerificado.append('email-prov', datosProveedor.email);        
+        datosProveedorVerificado.append('cuit-prov', datosProveedor.cuit);        
+        datosProveedorVerificado.append('telefono-prov', datosProveedor.telefono);        
+    
         fetch(URL + "proveedor" , {
                 method: "POST",
-                body: datosProveedor
+                body: datosProveedorVerificado
             })
         .then(respuesta => {
             console.log("Estado del POST: ", respuesta.status);    
@@ -191,8 +183,6 @@ formularioAgregarProveedor.addEventListener('submit', evento => {
                 alert("Ya existe el proveedor con CUIT:", cuit);
                 throw error;
             }
-            //respuesta.json();
-            //console.log("Respuesta del POST: ", respuesta);
         })
         .then(mensaje => {
             alert("Proveedor agregado exitosamente.");
@@ -213,7 +203,7 @@ function validarNombreProveedor(nombre) {
 }
 
 function validarDireccionProveedor(direccion) {
-    let expreg = /^[\wÑñ]{1,}[ ][\wÑñ ]{1,}/i; // Inicie con letra o número, mínimo 2 palabras (Calle y Nro) y puede contener ñ
+    let expreg = /^[\wÑñ]{1,}[ ][\wÑñ -]{1,}/i; // Inicie con letra o número, mínimo 2 palabras (Calle y Nro) y puede contener ñ
     return expreg.test(direccion); 
 }
 
@@ -223,7 +213,7 @@ function validarEmailProveedor(email) {
 }
 
 function validarCuitProveedor(cuit) {
-    let expreg = /[0-9]{2}[-][0-9]{8}[-][0-9]{1}/i; // Debe seguir el patrón X-XXXXXXXX-X
+    let expreg = /[0-9]{2}[-][0-9]{8}[-][0-9]{1}/i; // Debe seguir el patrón XX-XXXXXXXX-X
     return expreg.test(cuit); 
 }
 
@@ -234,8 +224,117 @@ function validarTelefonoProveedor(telefono) {
 
 
 
+/* --------------------------------Modificar Proveedor-------------------------------- */
+// Para modificar un proveedor desde el menú desplegable primeramente habrá que buscarlo
+let linkModificarProveedor = document.querySelector("#modificar-proveedor");
+linkModificarProveedor.addEventListener('click', generarPanelBuscarProveedor);
+
+// Por otra parte programo la modificación de información del proveedor
+let botonModificarProveedor = document.querySelector("#boton-modificar-proveedor");
+
+botonModificarProveedor.addEventListener("click", modificarProveedor);
+function modificarProveedor() {
+    let cuit = divResultado.querySelector("#cuit-proveedor-hallado").textContent;
+
+    fetch(URL + 'proveedor/' + cuit)
+    .then(datos => datos.json())
+    .then(proveedor => {
+        console.log("Proveedor a editar:", proveedor);
+
+        seccionEditarProveedor.style.display = "block";
+        document.querySelector("#id-prov-igual").textContent = proveedor.id;
+        document.querySelector("#nombre-prov-nuevo").value = proveedor.nombre;
+
+        if (proveedor.direccion == "") {
+            document.querySelector("#direccion-prov-nueva").value = "";
+            document.querySelector("#direccion-prov-nueva").setAttribute("placeholder", "Calle 000 - Provincia");
+        } else {document.querySelector("#direccion-prov-nueva").value = proveedor.direccion;};
+        
+        if (proveedor.email == "") {
+            document.querySelector("#email-prov-nuevo").value = "";
+            document.querySelector("#email-prov-nuevo").setAttribute("placeholder", "mail@dominio.ej");
+        } else {document.querySelector("#email-prov-nuevo").value = proveedor.email;};
+
+        document.querySelector("#cuit-proveedor-igual").textContent = proveedor.cuit;
+
+        if (proveedor.telefono == "") {
+            document.querySelector("#telefono-prov-nuevo").value = "";
+            document.querySelector("#telefono-prov-nuevo").setAttribute("placeholder", "0333 154455666");
+        } else {document.querySelector("#telefono-prov-nuevo").value = proveedor.telefono;};
+    })
+    .catch(error => console.log("Error al editar la información del proveedor.", error))
+    
+}
+
+
+let formularioEditarProveedor = document.querySelector('#form-editar-proveedor');
+formularioEditarProveedor.addEventListener('submit', evento => {
+    evento.preventDefault();
+
+    /* Validar el formulario para editar el proveedor */
+    let nombre = document.querySelector('#form-editar-proveedor #nombre-prov-nuevo').value;
+    let direccion = document.querySelector('#form-editar-proveedor #direccion-prov-nueva').value;
+    let email = document.querySelector('#form-editar-proveedor #email-prov-nuevo').value;
+    let cuit = document.querySelector('#form-editar-proveedor #cuit-proveedor-igual').textContent;
+    let telefono = document.querySelector('#form-editar-proveedor #telefono-prov-nuevo').value;    
+
+    let datosProveedor = {
+        nombre: nombre,
+        direccion: direccion,
+        email: email,
+        cuit: cuit,
+        telefono: telefono  
+    };
+    console.log("Datos de proveedor:", datosProveedor);
+
+    let datosValidos = true;
+    datosValidos = validarDatosProveedor(datosProveedor, "editar");
+    console.log("Validación de datos de proveedor", validarDatosProveedor(datosProveedor, "editar"));
+
+    if(datosValidos) {
+        let datosProveedorVerificado = new FormData();
+        datosProveedorVerificado.append('nombre-prov-editado', datosProveedor.nombre);        
+        datosProveedorVerificado.append('direccion-prov-editada', datosProveedor.direccion);        
+        datosProveedorVerificado.append('email-prov-editado', datosProveedor.email);        
+        datosProveedorVerificado.append('cuit-prov-editado', datosProveedor.cuit);        
+        datosProveedorVerificado.append('telefono-prov-editado', datosProveedor.telefono);        
+    
+        fetch(URL + "proveedor" , {
+                method: "PUT",
+                body: datosProveedorVerificado
+            })
+        .then(respuesta => {
+            console.log("Estado del POST: ", respuesta.status);    
+            if(respuesta.status == 400) {
+                alert("No se puedo actualizar la información del proveedor");
+                throw error;
+            }
+        })
+        .then(mensaje => {
+            alert("Proveedor editado exitosamente.");
+            generarPanelBuscarProveedor();
+            //limpiarFormularioAgregarProveedor();
+        })
+        .catch(error => console.log("Error al editar el proveedor:", error))
+    }
+}
+)
+
+
+let botonCancelarEditarProveedor = document.querySelector("#form-editar-proveedor #cancelar-editar-proveedor");
+botonCancelarEditarProveedor.addEventListener("click", cancelarEditarProveedor);
+function cancelarEditarProveedor () {
+    seccionEditarProveedor.style.display = "none";
+}
+
+
 /* --------------------------------Eliminar Proveedor-------------------------------- */
-let botonEliminarProveedor = document.querySelector("#eliminar-proveedor");
+// Para eliminar un proveedor desde el menú desplegable primeramente habrá que buscarlo
+let linkEliminarProveedor = document.querySelector("#eliminar-proveedor");
+linkEliminarProveedor.addEventListener('click', generarPanelBuscarProveedor);
+
+// Por otra parte programo la eliminación de la información del proveedor
+let botonEliminarProveedor = document.querySelector("#boton-eliminar-proveedor");
 
 botonEliminarProveedor.addEventListener("click", eliminarProveedor);
 function eliminarProveedor(){
@@ -253,3 +352,59 @@ function eliminarProveedor(){
         .catch(error => console.log("Error al eliminar el proveedor:", error))
     }
 }
+
+function validarDatosProveedor(datosProveedor, formulario) {
+    if (!validarNombreProveedor(datosProveedor['nombre'])) {
+        if (formulario == "editar") {
+            document.querySelector("#form-editar-proveedor #nombre-prov-nuevo").focus();
+        } else if (formulario == "agregar") {
+            //console.log("ACA ENTRA 1");
+            document.querySelector("#form-agregar-proveedor #nombre-prov").focus();
+        }
+        return false;
+    }
+
+    if (datosProveedor['direccion'] != "" & (!validarDireccionProveedor(datosProveedor['direccion']))) {
+        if (formulario == "editar") {
+            document.querySelector("#form-editar-proveedor #direccion-prov-nueva").focus();
+        } else if (formulario == "agregar") {
+            //console.log("ACA ENTRA 2");
+            document.querySelector("#form-agregar-proveedor #direccion-prov").focus();}
+        return false;
+    }
+
+    if (datosProveedor['email'] != "" & (!validarEmailProveedor(datosProveedor['email']))) {
+        if (formulario == "editar") {
+            document.querySelector("#form-editar-proveedor #email-prov-nuevo").focus();
+        } else if (formulario == "agregar") {
+            //console.log("ACA ENTRA 3");
+            document.querySelector("#form-agregar-proveedor #email-prov").focus();}
+        return false;
+    }
+
+    if (!validarCuitProveedor(datosProveedor['cuit'])) {
+        if (formulario == "editar") {
+            console.log(datosProveedor['cuit']);
+        } else if (formulario == "agregar") {
+            //console.log("ACA ENTRA 4");
+            document.querySelector("#form-agregar-proveedor #cuit-prov").focus();}
+        return false;
+    }
+
+    if (datosProveedor['telefono'] != "" & (!validarTelefonoProveedor(datosProveedor['telefono']))) {
+        if (formulario == "editar") {
+            document.querySelector("#form-editar-proveedor #telefono-prov-nuevo").focus();
+        } else if (formulario == "agregar") {
+            //console.log("ACA ENTRA 5");
+            document.querySelector("#form-agregar-proveedor #telefono-prov").focus();}
+        return false;
+    }
+
+    return true;
+
+}
+/*
+if(false) {
+    console.log("No deberia entrar aca");
+} else if (true) {console.log("SI deberia entrar aca");}
+*/
